6e72537fa7bb88b9a4633cfca6535cdfe4c981b5
Hungvt08 <vothehung08@gmail.com>
2026-05-15 20:53:46 +0700

    update calculation function

--- src/CoreBanking.cpp
+++ src/CoreBanking.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Core Banking System - Version 1.0" << std::endl;
-    std::cout << "He thong da san sang de xử lý giao dich!" << std::endl;
+    double calculate(double a, double b);
     return 0;
 }
