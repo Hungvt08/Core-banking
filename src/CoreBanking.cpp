a0ab037993689f5b3345c2ecfbb7ac6b75379c72
Hungvt08 <vothehung08@gmail.com>
2026-05-15 20:09:14 +0700

    Feat: them tinh nang vao system

---
+++ src/CoreBanking.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Core Banking System - Version 1.0" << std::endl;
+    std::cout << "He thong da san sang de xử lý giao dich!" << std::endl;
+    return 0;
+}
